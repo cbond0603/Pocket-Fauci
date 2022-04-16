63638cbd4eeeedfc72d3f8166c5121af2a06fa1c
Chris Bond <bondcb@bc.edu>
2022-04-16 12:19:09 -0400

    Initial Commit

---
+++ Pocket Fauci/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pocket Fauci
+//
+//  Created by Chris Bond on 4/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
